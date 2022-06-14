7e53abcb52a6df86f6a06dbbead45a4e6d5c2f09
keddib <keddib@student.1337.ma>
2022-06-14 20:15:02 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Login from "./Loging/index";
 import Dashboard from "./Dashboard/index";
-// import Home from "./Home/Home";
+import Home from "./Dashboard/MainPage/Home";
+import LeaderBoard from "./Dashboard/MainPage/LeaderBoard";
+import Messages from "./Dashboard/MainPage/Messages";
+import Friends from "./Dashboard/MainPage/Friends";
+import Groups from "./Dashboard/MainPage/Groups";
+import Profile from "./Dashboard/MainPage/Profile";
+import Landing from "./Home/Home";
 
 const App = () => {
   return (
@@ -12,7 +18,15 @@ const App = () => {
         {/* <Route path="/details/:id" element={<Details />} /> */}
         <Route path="/signin" element={<Login stage={true} />} />
         <Route path="/signup" element={<Login />} />
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/home" element={<Landing />} />
+        <Route path="/" element={<Dashboard />}>
+          <Route index element={<Home />} />
+          <Route path="leaderboard" element={<LeaderBoard />} />
+          <Route path="messages" element={<Messages />} />
+          <Route path="friends" element={<Friends />} />
+          <Route path="groups" element={<Groups />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
