016ecc0306e9c087e8b9ed437cf7e34cb15d6adc
keddib <keddib@student.1337.ma>
2022-06-14 20:14:22 +0100

    user wrapper with name and status will be used all cross pages

---
+++ src/Components/UserWrapper.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+export default function UserWrapper({ user }) {
+  return (
+    <Link to={user.id} className="user-wrapper group">
+      <div className="relative">
+        <img alt="user" src={user.img} className="user-img" />
+        <span className={user.dot}></span>
+      </div>
+      <div className="group-hover:text-lotion/70">
+        <h4 >{user.name}</h4>
+        <p >{user.status}</p>
+      </div>
+    </Link >
+  );
+}
