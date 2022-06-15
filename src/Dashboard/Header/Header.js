8b53c1cacf20e1b77b5ba6c2752bf000cb587c38
keddib <keddib@student.1337.ma>
2022-06-15 18:56:33 +0100

    complete the dashboard header

--- src/Dashboard/Header/Header.js
+++ src/Dashboard/Header/Header.js
@@ -1,12 +1,35 @@
 import Profile from "../../Components/UserWrapper";
-import Bell from "../../../public/assets/icons/bell.svg"
+import Bell from "../../../public/assets/icons/bell.svg";
+import Star from "../../../public/assets/icons/star.svg";
+
+function EX() {
+  const userEX = {
+    lvl: 12,
+    ex: 1537,
+  };
+
+  return (
+    <div className="EX">
+      <Star className="fill-crayola w-14 mr-4" />
+      <p className=" font-beaufort font-semibold text-2xl absolute top-4 left-3">
+        {userEX.lvl}
+      </p>
+      <div className="flex flex-col justify-center grow ">
+        <p>{`${userEX.ex}xp`}</p>
+        <div className="w-full h-3 rounded-3xl bg-lotion/50">
+          <div className="w-2/3 bg-crayola h-3 rounded-3xl"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default function Headers({ user }) {
   return (
-    <div className="DashHeader">
-      <div className="EX">lol</div>
-      <div className="grow"></div>
-      <div className="flex justify-between items-center w-60">
+    <div className="DashHeader w-full">
+      <EX />
+      <div className="md:grow"></div>
+      <div className="flex justify-between items-center w-full md:w-60">
         <Profile user={user} />
         <button className="group">
           <Bell className="iconBell" />
