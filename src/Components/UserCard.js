c457e5574f1dfbb197947d6e7143166e9e7bbb08
keddib <keddib@student.1337.ma>
2022-06-22 19:55:19 +0100

    add error page

--- src/Components/UserCard.js
+++ src/Components/UserCard.js
@@ -1,6 +1,13 @@
 import { Link } from "react-router-dom";
 
 export default function UserCard({ user }) {
+
+  let name = user.name;
+  if (user.name.length > 18) {
+    name = user.name.substring(0, 17);
+    name += '.';
+  }
+
   return (
     <Link to={user.id} className="user-wrapper group">
       <div className="relative">
@@ -8,7 +15,7 @@ export default function UserCard({ user }) {
         <span className={user.statusColor}></span>
       </div>
       <div className="group-hover:text-lotion/70 ml-4">
-        <h4>{user.name}</h4>
+        <h4>{name}</h4>
         <p>{user.status}</p>
       </div>
     </Link>
