0adeec746c03edd670a344e8cbab24b7bc4bda5e
keddib@student.1337.ma <keddib@student.1337.ma>
2022-09-19 23:58:54 +0100

    add spectating to freinds section

--- src/features/Friends/components/Friends/FriendListItem.tsx
+++ src/features/Friends/components/Friends/FriendListItem.tsx
@@ -7,6 +7,9 @@ import { User } from "types/app";
 import { useLocation, useNavigate } from "react-router-dom";
 import useAxiosPrivate from "~/src/hooks/useAxiosPrivate";
 import useAuth from "~/src/hooks/useAuth";
+import useUserStatus from "~/src/hooks/useUserStatus";
+import Spectate from "~/src/components/Spectate";
+
 const FriendListItem: FunctionComponent<{ user: User }> = ({ user }) => {
   const navigate = useNavigate();
   const axiosPrivate = useAxiosPrivate();
@@ -14,6 +17,15 @@ const FriendListItem: FunctionComponent<{ user: User }> = ({ user }) => {
   const auth = useAuth();
   const location = useLocation();
   const from = location.pathname;
+  const { userStatus } = useUserStatus();
+  const [status, setStatus] = useState(user.status);
+
+  useEffect(() => {
+    if (userStatus.userId == user.uid) {
+      setStatus(userStatus.status);
+      user.status = userStatus.status;
+    }
+  }, [userStatus, setStatus, user]);
 
   const handleInviteToPlayButton = () => {
     const gameMode = "classic";
@@ -50,14 +62,16 @@ const FriendListItem: FunctionComponent<{ user: User }> = ({ user }) => {
         <div className="flex items-center gap-4 sm:gap-8 sm:mr-8">
           {!unblock && (
             <>
-              {
+              {status == "playing" ? (
+                <Spectate userId={user.uid} />
+              ) : (
                 <button
                   className="send game request"
                   onClick={handleInviteToPlayButton}
                 >
                   <GamePad className="w-6 h-6 sm:w-8 sm:h-8 fill-lotion/50 hover:fill-lotion ease-in duration-150" />
                 </button>
-              }
+              )}
               <button className="start chating" onClick={hundleSendMessage}>
                 <DmIcon className="w-6 h-4 sm:w-8 sm:h-6 fill-lotion/50 hover:fill-lotion ease-in duration-150" />
               </button>
