040107ef236ab346149e55cc7a40a88a99bd54a6
keddib@student.1337.ma <keddib@student.1337.ma>
2022-09-20 10:29:14 +0100

    serv production with express

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-npm run dev
+npm run build
+npm run production
---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+const PORT = process.env.FRONT_PORT || 80;
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../dist")));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
